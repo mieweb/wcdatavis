7b55c2cec7e7ef9e1c2fd7c2ff7c9b81aed3a3db
Taylor Venable <tvenable@mieweb.com>
2026-04-01 14:10:51 -0400

    Fix: Filter `$contains` op didn't work when matching a value with upper-case letters in it.

--- src/computed_view.js
+++ src/computed_view.js
@@ -1461,7 +1461,7 @@ ComputedView.prototype.filter = function (cont) {
 		};
 
 		pred['$contains'] = function (operand) {
-			return isString && datum.indexOf(operand.toString().toLowerCase()) >= 0;
+			return isString && datum.toLowerCase().indexOf(operand.toString().toLowerCase()) >= 0;
 		};
 
 		pred['$notcontains'] = function (operand) {
